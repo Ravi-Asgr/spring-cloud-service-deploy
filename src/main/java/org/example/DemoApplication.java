71ad31a8308c0a90405ddf51db5fc150b4b0f3ac
Codefather <ravi063@gmail.com>
2026-06-22 20:28:14 +0530

    Comment to main class

--- src/main/java/org/example/DemoApplication.java
+++ src/main/java/org/example/DemoApplication.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*Main Class*/
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
